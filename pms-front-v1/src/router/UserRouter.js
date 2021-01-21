4cc6c81a72b7dbc973624596c6c5e2485db6ad73
강은빈 <kub0803@naver.com>
2021-01-21 17:30:48 +0900

    [ADD] Header Add

---
+++ pms-front-v1/src/router/UserRouter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import { Header } from "../components/index";
+
+function UserRouter() {
+  return (
+    <Switch>
+      <Route path="/" component={} exact />
+    </Switch>
+  );
+}
+
+export default UserRouter;
