fed2841f83d1bd01d285f50d8c88311a75c232b7
강은빈 <kub0803@naver.com>
2021-01-27 23:48:17 +0900

    [MODIFY] 헤더 스타일 수정

--- pms-front-v1/src/components/Header/style.js
+++ pms-front-v1/src/components/Header/style.js
@@ -6,6 +6,7 @@ const Header = styled.div`
   width: 100%;
   height: 60px;
   background-color: rgba(1, 4, 3, 0.27);
+  z-index: 1;
 `;
 
 const Nav = styled.div`
@@ -17,7 +18,7 @@ const Nav = styled.div`
   align-items: center;
   justify-content: space-around;
   flex-direction: row;
-  margin-right: 14%;
+  margin-right: 9%;
 `;
 
 const Link = styled.span`
