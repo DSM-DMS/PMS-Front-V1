d78847a7300ca72a2b1aba9c178e5e0a104abfa4
강은빈 <kub0803@naver.com>
2021-01-27 23:46:51 +0900

    [MODIFY] 이미지 추가

--- pms-front-v1/src/assets/index.js
+++ pms-front-v1/src/assets/index.js
@@ -1,3 +1,4 @@
-export { default as MainBackground } from './MainBackground.svg';
-export { default as Study } from './Study.svg';
-export { default as DMS } from './DMS.svg';
+export { default as MainBackground } from "./MainBackground.svg";
+export { default as Study } from "./Study.svg";
+export { default as DMS } from "./DMS.svg";
+export { default as Company } from "./Company.svg";
