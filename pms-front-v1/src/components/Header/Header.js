8295ad0dea116f9adce9cdc237669564c8e2945d
강은빈 <kub0803@naver.com>
2021-02-05 02:17:12 +0900

    [MODIFY] 헤더 수정

--- pms-front-v1/src/components/Header/Header.js
+++ pms-front-v1/src/components/Header/Header.js
@@ -1,20 +1,15 @@
 import React from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import * as S from "./style";
 
 const Header = () => {
   return (
     <S.Header>
       <S.Nav>
-        {/* <Link to="/">학교 소식</Link>
+        <Link to="/">학교소식</Link>
         <Link to="/">소개</Link>
         <Link to="/">마이페이지</Link>
-        <Link to="/">로그인</Link> */}
-        
-        <S.Link>학교소식</S.Link>
-        <S.Link>소개</S.Link>
-        <S.Link>마이페이지</S.Link>
-        <S.Link>로그인</S.Link>
+        <Link to="login">로그인</Link>
       </S.Nav>
     </S.Header>
   );
