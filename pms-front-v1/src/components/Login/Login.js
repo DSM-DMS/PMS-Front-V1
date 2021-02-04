756715cb4e45919d9cb47e0b4e0f76c52b1d595a
강은빈 <kub0803@naver.com>
2021-02-05 01:23:26 +0900

    [MODIFY] 로그인 스타일 변경

--- pms-front-v1/src/components/Login/Login.js
+++ pms-front-v1/src/components/Login/Login.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import * as S from "./style";
 
 import BackgroundTitle from "../BackgroundTitle";
+import SocialButton from "./SocialButton";
 
 function Login() {
   return (
     <S.MainWrapper>
       <BackgroundTitle title="로그인" />
+      {/* 로그인 박스 */}
       <S.LoginWrapper>
         <S.MainItem>
-					<S.Title>
-						<p>PMS 로그인</p>
-						<Link to="sign-up">회원가입 하기 > </Link>
-					</S.Title>
-					<S.LoginInput>
-						<input placeholder="이메일"></input>
-						<input placeholder="비밀번호"></input>
-						<button>로그인</button>
-					</S.LoginInput>
-				</S.MainItem>
+          <S.Title>
+            <p>PMS 로그인</p>
+            <Link to="sign-up">회원가입 하기 > </Link>
+          </S.Title>
+          {/* 로그인 입력창 */}
+          <S.LoginInput>
+            <S.InputWrapper>
+              <input type="text" placeholder="이메일"></input>
+              <input type="password" placeholder="비밀번호"></input>
+              <div className="auto-login">
+                <input type="checkbox"></input>
+                <span>자동 로그인</span>
+              </div>
+              <button>로그인</button>
+            </S.InputWrapper>
+            {/* 소셜 로그인 */}
+            <S.SocialWrapper>
+              <SocialButton LoginName="FaceBook 로그인" />
+              <SocialButton LoginName="Google 로그인" />
+              <SocialButton LoginName="KaKaotalk 로그인" />
+              <SocialButton LoginName="Naver 로그인" />
+            </S.SocialWrapper>
+          </S.LoginInput>
+        </S.MainItem>
       </S.LoginWrapper>
     </S.MainWrapper>
   );
--- pms-front-v1/src/components/Login/style.js
+++ pms-front-v1/src/components/Login/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const MainWrapper = styled.div`
   height: 1230px;
-  background: #F6F6F6 0% 0% no-repeat padding-box;
+  background: #f6f6f6 0% 0% no-repeat padding-box;
 `;
 
 const LoginWrapper = styled.div`
@@ -13,22 +13,118 @@ const LoginWrapper = styled.div`
   height: 800px;
   background-color: white;
   box-shadow: 0px 5px 5px #00000029;
-  display:flex;
+  display: flex;
   justify-content: center;
   align-items: center;
-
 `;
 
 const MainItem = styled.div`
-magin: auto 0;
-    width: 600px;
-    height: 400px;
-    border: 1px solid red;
-    
+  margin: auto 0;
+  width: 600px;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const Title = styled.div``;
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-weight: 600;
+  p {
+    font-size: 35px;
+  }
+  a {
+    font-size: 15px;
+    color: #4c75b2;
+    text-decoration: none;
+  }
+`;
 
+const LoginInput = styled.div`
+  margin: 30px 0;
+  display: flex;
+  flex-direction: row;
 
+  justify-content: center;
+  width: 100%;
+  height: 80%;
+`;
 
-export { MainWrapper, LoginWrapper, MainItem, Title};
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-right: 1.5px solid #9e9e9e;
+
+  input {
+    margin: 0 20px 15px 0;
+    border: 0;
+    border-bottom: 1px solid #c8c8c8;
+    outline: none;
+    width: 280px;
+    height: 30px;
+    font-size: 18px;
+  }
+
+  button {
+    margin-top: 50px;
+    width: 200px;
+    height: 40px;
+    background: #4c75b2 0% 0% no-repeat padding-box;
+    border-radius: 5px;
+    color: white;
+    outline: none;
+    border: none;
+    font-size: 16px;
+    //font-weight: 600;
+  }
+
+  //자동 로그인
+  span {
+    margin: 0 0 5px 0;
+    font-size: 15px;
+  }
+
+  .auto-login {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    color: #9e9e9e;
+  }
+  .auto-login input {
+    margin: 5px;
+    width: 20px;
+    height: 20px;
+    border: 1px solid #c8c8c8;
+    border-radius: 5px;
+  }
+`;
+
+const SocialWrapper = styled.div`
+  width: 100%;
+  height: 60%;
+  margin-top: 30px;
+  margin-left: 30px;
+`;
+
+const LoginButton = styled.div`
+  width: 240px;
+  height: 40px;
+  border: ${({ LoginName }) => LoginName};
+`;
+
+export {
+  MainWrapper,
+  LoginWrapper,
+  LoginInput,
+  MainItem,
+  Title,
+  InputWrapper,
+  SocialWrapper,
+  //소셜 로그인
+  LoginButton,
+};
