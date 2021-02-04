b7a56a56f7598ef37c1cadfc88aafb968cd0da08
강은빈 <kub0803@naver.com>
2021-02-05 01:22:48 +0900

    [ADD] Footer 추가

---
+++ pms-front-v1/src/components/footer/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const FooterWarpper = styled.div`
+  width: 100%;
+  height: 150px;
+  background: #f6f6f6 0% 0% no-repeat padding-box;
+  
+`;
+
+export { FooterWarpper };
