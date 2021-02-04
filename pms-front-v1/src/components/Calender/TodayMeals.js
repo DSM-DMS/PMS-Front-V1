c6003a9ecaeaf0c99366d2b8e16937d92ebd08b4
강은빈 <kub0803@naver.com>
2021-02-05 02:44:33 +0900

    [ADD] 오늘의 급식 컴포넌트 추가

---
+++ pms-front-v1/src/components/Calender/TodayMeals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+function TodayMeals() {
+  return (
+    <S.SideWrapper>
+      <S.Title>오늘의 급식</S.Title>
+    </S.SideWrapper>
+  );
+}
+
+export default TodayMeals;
