ef4453267059439146c2f20179fa40c906ba1c6e
강은빈 <kub0803@naver.com>
2021-02-06 00:02:10 +0900

    [MODIFY] 스타일 수정

--- pms-front-v1/src/components/Calender/style.js
+++ pms-front-v1/src/components/Calender/style.js
@@ -134,6 +134,13 @@ const MealButton = styled.button`
   font-size: 16px;
 `;
 
+const CalenderMain = styled.div`
+margin: 15px;
+  width : 700px;
+  height : 630px;
+  border: 1px solid red;
+`;
+
 export {
   MainWrapper,
   CalenderWrapper,
@@ -148,4 +155,5 @@ export {
   MealsList,
   Nav,
   MealButton,
+  CalenderMain,
 };
