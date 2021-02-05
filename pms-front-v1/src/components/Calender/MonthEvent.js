0ad3979246302e29e6a438920ae729f204f5c2f5
강은빈 <kub0803@naver.com>
2021-02-05 18:27:05 +0900

    [MODIFY] 이달의 행사 컴포넌트 완성

--- pms-front-v1/src/components/Calender/MonthEvent.js
+++ pms-front-v1/src/components/Calender/MonthEvent.js
@@ -1,10 +1,13 @@
 import React from "react";
 import * as S from "./style";
 
+import EventList from "./EventList";
+
 function MonthEvent() {
   return (
     <S.SideWrapper>
       <S.Title>이달의 행사</S.Title>
+      <EventList />
     </S.SideWrapper>
   );
 }
--- pms-front-v1/src/components/Calender/style.js
+++ pms-front-v1/src/components/Calender/style.js
@@ -21,19 +21,72 @@ const SideWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 300px;
+  width: 21%;
   height: 100%auto;
   background: #ffffff 0% 0% no-repeat padding-box;
   box-shadow: 0px 5px 5px #00000029;
 `;
 
 const Title = styled.div`
+  margin: 25px;
   font-size: 20px;
+  font-weight: 600;
 `;
 
-const MiddleWrapper =styled(SideWrapper)`
-    width: 750px;
+const MiddleWrapper = styled(SideWrapper)`
+  width: 53%;
 `;
 
+const Event = styled.div`
+  margin: 15px;
+  width: 80%;
+  height: 60px;
+  background: #f6f6f6 0% 0% no-repeat padding-box;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+`;
 
-export { MainWrapper, CalenderWrapper, SideWrapper, Title, MiddleWrapper };
+const EventName = styled.div`
+  margin: 10px 20px;
+  width: 44%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  .circle {
+    width: 16px;
+    height: 16px;
+    background-color: ${({ EventColor }) => EventColor};
+    border-radius: 50px;
+  }
+  span {
+    margin-left: 10px;
+    font-size: 14px;
+    //font-weight: 600;
+  }
+`;
+
+const EventDate = styled.div`
+  height: 48px;
+  font: normal normal normal 14px/14px Noto Sans CJK KR;
+  letter-spacing: 0px;
+  color: #7d7d7d;
+  display:flex;
+  align-items:center;
+  span{
+    margin:15px;
+  }
+`;
+
+export {
+  MainWrapper,
+  CalenderWrapper,
+  SideWrapper,
+  Title,
+  MiddleWrapper,
+  Event,
+  EventName,
+  EventDate,
+};
