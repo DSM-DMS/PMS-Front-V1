f5cfdd256a8ef47fe38015020b09dc6cd9d0d79f
강은빈 <kub0803@naver.com>
2021-02-06 23:59:37 +0900

    [MODIFY] 소개 페이지 상단 헤더 컴포넌트 완성

--- pms-front-v1/src/components/Infomation/InfoHeader.js
+++ pms-front-v1/src/components/Infomation/InfoHeader.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import * as S from './style';
+import React from "react";
+import * as S from "./style";
 
-const InfoHeader = () => {
-    return (
-			<S.MainWrapper></S.MainWrapper>
-    )
-}
+const InfoHeader = (props) => {
+  return (
+    <S.InfoHeaderWrapper>
+      <h1>{props.title}</h1>
+      <div className="info-search">
+        <span>이름</span>
+        <input type="text" placeholder={props.placeholder} />
+      </div>
+    </S.InfoHeaderWrapper>
+  );
+};
 
 export default InfoHeader;
