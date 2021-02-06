9fa148d12ce68334740a92eea0502a29b6eb9ead
강은빈 <kub0803@naver.com>
2021-02-06 23:58:11 +0900

    [MODIFY] 동아리 소개페이지 완성

--- pms-front-v1/src/components/Infomation/Club/ClubInfo.js
+++ pms-front-v1/src/components/Infomation/Club/ClubInfo.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import * as S from '../style';
+import React from "react";
+import * as S from "../style";
+
+import BackgroundTitle from "../../BackgroundTitle";
+import Footer from "../../footer/Footer";
+import InfoHeader from "../InfoHeader";
+import InfoItemBox from "../InfoItemBox";
 
 function ClubInfo() {
-    return ( 
-			<S.MainWrapper></S.MainWrapper>
-		)
+  const clubList = [
+    {
+      clubName: "DMS",
+      clubInfomation:
+        "기숙사 지원 시스템을 만들고 있습니다. DSM학생들의 원활한 기숙사 생활을 위하여!",
+    },
+		{
+      clubName: "DMS",
+      clubInfomation:
+        "기숙사 지원 시스템을 만들고 있습니다. DSM학생들의 원활한 기숙사 생활을 위하여!",
+    },
+		{
+      clubName: "DMS",
+      clubInfomation:
+        "기숙사 지원 시스템을 만들고 있습니다. DSM학생들의 원활한 기숙사 생활을 위하여!",
+    },
+		{
+      clubName: "DMS",
+      clubInfomation:
+        "기숙사 지원 시스템을 만들고 있습니다. DSM학생들의 원활한 기숙사 생활을 위하여!",
+    },
+		{
+      clubName: "DMS",
+      clubInfomation:
+        "기숙사 지원 시스템을 만들고 있습니다. DSM학생들의 원활한 기숙사 생활을 위하여!",
+    },
+		{
+      clubName: "DMS",
+      clubInfomation:
+        "기숙사 지원 시스템을 만들고 있습니다. DSM학생들의 원활한 기숙사 생활을 위하여!",
+    },
+		
+
+  ];
+  return (
+    <S.InfoMainWrapper>
+      <BackgroundTitle title="" />
+      <InfoHeader title="동아리 소개" placeholder="동아리를 입력해주세요" />
+      <S.ItemBoxWrapper>
+        {clubList.map((club) => {
+          return (
+            <InfoItemBox
+              clubName={club.clubName}
+              clubInfomation={club.clubInfomation}
+            />
+          );
+        })}
+      </S.ItemBoxWrapper>
+      <Footer />
+    </S.InfoMainWrapper>
+  );
 }
 
 export default ClubInfo;
--- pms-front-v1/src/components/Infomation/style.js
+++ pms-front-v1/src/components/Infomation/style.js
@@ -1,8 +1,14 @@
 import styled from "styled-components";
 
 const MainWrapper = styled.div`
-  width: 100%auto;
+  width: 100%;
   height: 1230px;
+  background: #f6f6f6 0% 0% no-repeat padding-box;
+`;
+
+const InfoMainWrapper = styled.div`
+  width: 100%;
+  height: 1430px;
   background: #f6f6f6 0% 0% no-repeat padding-box;
 `;
 
@@ -33,4 +39,95 @@ const InfoItem = styled.div`
   }
 `;
 
-export { MainWrapper, CreatorsWrapper, InfoItem };
+const InfoHeaderWrapper = styled.div`
+  position: relative;
+  top: -7%;
+  margin: 0 auto;
+  width: 78%;
+  height: 176px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 5px 5px #00000029;
+
+  h1 {
+    font-size: 26px;
+    font-weight: 600;
+    margin-left: 70px;
+  }
+
+  .info-search {
+    margin-left: 70px;
+    margin-top: 25px;
+    display: flex;
+    flex-direction: row;
+    width: 340px;
+    height: 40px;
+    border: 1px solid #c8c8c8;
+  }
+  .info-search span {
+    width: 120px;
+    padding: 10px;
+    border-right: 1px solid #c8c8c8;
+  }
+
+  .info-search input {
+    outline: none;
+    width: 100%;
+    border: none;
+    padding: 10px;
+  }
+`;
+
+const ItemBoxWrapper = styled.div`
+  position: relative;
+  top: -4%;
+  margin: 0 auto;
+  width: 70%;
+  height: 800px;
+  display: flex;
+  flex-wrap: wrap;
+	align-content: baseline;
+  overflow: auto;
+`;
+
+const ItemBox = styled.div`
+  margin: 20px;
+  width: 290px;
+  height: 200px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 5px 5px #00000029;
+  border: 1px solid #c8c8c8;
+  border-radius: 5px;
+`;
+
+const ItemContainer = styled.div`
+  width: 270px;
+  height: 180px;
+  margin: 10px;
+  img {
+    width: 100%;
+    height: 100px;
+    border: 1px solid red;
+  }
+  h3 {
+    font-size: 16px;
+    font-weight: 600;
+  }
+  span {
+    font-size: 14px;
+    color: #545454;
+  }
+`;
+
+export {
+  MainWrapper,
+  InfoMainWrapper,
+  CreatorsWrapper,
+  InfoItem,
+  InfoHeaderWrapper,
+  ItemBoxWrapper,
+  ItemBox,
+  ItemContainer,
+};
