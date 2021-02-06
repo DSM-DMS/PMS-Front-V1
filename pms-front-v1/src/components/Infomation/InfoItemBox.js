e30e12f788becd1a0722eb7479cb2d5a98e3e6da
강은빈 <kub0803@naver.com>
2021-02-07 00:34:45 +0900

    [MODIFY] 소개 아이템 박스 수정

--- pms-front-v1/src/components/Infomation/InfoItemBox.js
+++ pms-front-v1/src/components/Infomation/InfoItemBox.js
@@ -5,9 +5,10 @@ const InfoItemBox = (props) => {
   return (
     <S.ItemBox>
       <S.ItemContainer>
-        <img scr="/"></img>
-        <h3>{props.clubName}</h3>
-				<span>{props.clubInfomation}</span>
+        <img scr="{props.clubImg}"></img>
+        <h3>{props.title}</h3>
+				<p>{props.explanation}</p>
+				<span>{props.resolution}</span>
       </S.ItemContainer>
     </S.ItemBox>
   );
