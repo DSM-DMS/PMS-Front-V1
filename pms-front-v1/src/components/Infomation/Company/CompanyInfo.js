36dfb33368f24909344303d8da0daf0f170d4648
강은빈 <kub0803@naver.com>
2021-02-07 00:36:10 +0900

    [MODIFY] 개발자 소개 위치 변경

--- pms-front-v1/src/components/Infomation/Company/CompanyInfo.js
+++ pms-front-v1/src/components/Infomation/Company/CompanyInfo.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import * as S from '../style';
+import React from "react";
+import * as S from "../style";
 
-function CompanyInfo() {
-    return (
-			<S.MainWrapper></S.MainWrapper>
-		)
-}
+const CompanyInfo = (props) => {
+  return <S.MainWrapper></S.MainWrapper>;
+};
 
 export default CompanyInfo;
