eec1993563b5c5ab46c9ec59932c155c9709d79d
강은빈 <kub0803@naver.com>
2021-02-13 00:14:08 +0900

    [ADD] 소셜로그인  이미지 추가

--- pms-front-v1/src/assets/index.js
+++ pms-front-v1/src/assets/index.js
@@ -1,3 +1,11 @@
-export { default as MainBackground } from './MainBackground.svg';
-export { default as Study } from './Study.svg';
-export { default as DMS } from './DMS.svg';
+export { default as MainBackground } from "./MainBackground.svg";
+export { default as Study } from "./Study.svg";
+export { default as DMS } from "./DMS.svg";
+export { default as Company } from "./Company.svg";
+export { default as PMS } from "./PMS.svg";
+export { default as Info } from "./Info.svg";
+export { default as MyPage } from "./MyPage.svg";
+export { default as Close } from "./Close.png";
+export { default as FaceBook } from "./FaceBook.png";
+export { default as kakao } from "./kakao.svg";
+export { default as Google } from "./Google.svg";
