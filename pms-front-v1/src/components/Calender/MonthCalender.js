e2d1240535a2e5e5f709fdb2d023b3536a2cb757
강은빈 <kub0803@naver.com>
2021-03-08 23:13:34 +0900

    [MODIFY] 이달의 캘린더 수정

--- pms-front-v1/src/components/Calender/MonthCalender.js
+++ pms-front-v1/src/components/Calender/MonthCalender.js
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 import * as S from "./style";
 import Calendar from "react-calendar";
-//import 'react-calendar/dist/Calendar.css';
 
 function MonthCalender() {
   const [value, onChange] = useState(new Date());
 
   return (
     <S.MiddleWrapper>
-      <S.Title>
-      </S.Title>
       <S.CalenderMain>
-        <Calendar onChange={onChange} value={value} />
+        <Calendar onChange={onChange} value={value}  />
       </S.CalenderMain>
     </S.MiddleWrapper>
   );
