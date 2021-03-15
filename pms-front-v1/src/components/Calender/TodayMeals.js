4811504cc0757f662f1218bb13a2d0e274311848
강은빈 <kub0803@naver.com>
2021-03-15 23:45:23 +0900

    [MODIFY] 오늘의 급식 API 추가

--- pms-front-v1/src/components/Calender/TodayMeals.js
+++ pms-front-v1/src/components/Calender/TodayMeals.js
@@ -1,28 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
 
 import EventList from "./EventList";
+import { requestJW } from "../../utils/axios/axios";
 
 function TodayMeals() {
   //버튼이 선택되었는지 확인하는 변수
   const [selected, setSelected] = useState(0);
+  const [todayMeals, setTodayMeals] = useState("");
 
-  //오늘의 날짜 
+  //오늘의 날짜
   const date = new Date();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const year = date.getFullYear();
+  const month = ("0" + (1 + date.getMonth())).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+
+  let TodayDate = year + "" + month + "" + day;
+
+  console.log(TodayDate);
 
   function getTodayLabel() {
-    
-    var week = new Array('일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일');
-    
+    var week = new Array(
+      "일요일",
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일"
+    );
+
     var today = new Date().getDay();
     var todayLabel = week[today];
-    
+
     return todayLabel;
   }
 
-  console.log(getTodayLabel(date))
+  const getMeal = async () => {
+    try {
+      const { data } = await requestJW(
+        "get",
+        `event/meal/${TodayDate}`,
+        { Authorization: `Bearer ${localStorage.getItem("access-token")}` },
+        {}
+      );
+      setTodayMeals(data);
+      console.log(setTodayMeals);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getMeal();
+  }, []);
 
   //오늘의 급식 더미데이터
   const meals = [
@@ -64,7 +95,9 @@ function TodayMeals() {
   return (
     <S.SideWrapper>
       <S.Title>오늘의 급식</S.Title>
-      <S.SelectData>{month}월 {day}일 {getTodayLabel(date)}</S.SelectData>
+      <S.SelectData>
+        {month}월 {day}일 {getTodayLabel(date)}
+      </S.SelectData>
       <S.MealsList>
         {meals.map((meal) => {
           return <span>{meal.menu}</span>;
