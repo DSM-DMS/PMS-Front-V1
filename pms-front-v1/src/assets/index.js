c2b469f25ef601b94e6ef249d3f4761a6b58e226
Unknown <ljw041101@gmail.com>
2021-03-19 19:52:13 +0900

    [MODIFY] index.js 수정

--- pms-front-v1/src/assets/index.js
+++ pms-front-v1/src/assets/index.js
@@ -2,12 +2,14 @@ export { default as MainBackground } from "./MainBackground.svg";
 export { default as Study } from "./Study.svg";
 export { default as DMS } from "./DMS.svg";
 export { default as Company } from "./Company.svg";
-export { default as PMS } from "./PMS.svg";
-export { default as Info } from "./Info.svg";
-export { default as MyPage } from "./MyPage.svg";
-export { default as Close } from "./Close.png";
-export { default as FaceBook } from "./FaceBook.png";
-export { default as kakao } from "./kakao.svg";
-export { default as Google } from "./Google.svg";
-export { default as Earth } from "./Earth.jpg";
-
+export { default as PMS } from './PMS.svg';
+export { default as Info } from './Info.svg';
+export { default as MyPage} from './MyPage.svg';
+export { default as Close } from './Close.png';
+export { default as Profile } from './Prifile.svg'
+export { default as Bike } from './Bike.svg'
+export { default as Home } from './Home.svg'
+export { default as MealGreen} from './MealGreen.svg'
+export { default as RightPassword} from './RightPassword.svg'
+export { default as AddChildren} from './AddChildren.svg'
+export { default as Triangle} from './Triangle.svg'
