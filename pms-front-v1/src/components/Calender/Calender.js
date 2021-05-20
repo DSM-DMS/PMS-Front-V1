2a5c85c29c741f6ddf633434541f7c8e73bdfb8f
강은빈 <kub0803@naver.com>
2021-05-20 23:51:20 +0900

    [MODIFY] 이달의 캘린더 API 받아오기

--- pms-front-v1/src/components/Calender/Calender.js
+++ pms-front-v1/src/components/Calender/Calender.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
 
 import BackgroundTitle from "../BackgroundTitle";
@@ -6,13 +6,39 @@ import Footer from "../footer/Footer";
 import MonthEvent from "./MonthEvent";
 import MonthCalender from "./MonthCalender";
 import TodayMeals from "./TodayMeals";
+import { requestJW } from "../../utils/axios/axios";
 
 function Calender() {
+  const [eventData, setEventData] = useState([]);
+
+  let date = new Date();
+  let month = date.getMonth() + 1;
+
+  const eventApi = async () => {
+    try {
+      const { data } = await requestJW(
+        "get",
+        "calendar",
+        {
+          Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+        },
+        {}
+      );
+      setEventData(data[`${month}`]);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    eventApi();
+  }, []);
+
   return (
     <S.MainWrapper>
       <BackgroundTitle title="" />
       <S.CalenderWrapper>
-        <MonthEvent />
+        <MonthEvent eventList={eventData} />
         <MonthCalender />
         <TodayMeals />
       </S.CalenderWrapper>
--- pms-front-v1/src/components/Calender/MonthCalender.js
+++ pms-front-v1/src/components/Calender/MonthCalender.js
@@ -5,10 +5,19 @@ import Calendar from "react-calendar";
 function MonthCalender() {
   const [value, onChange] = useState(new Date());
 
+  const callDay = (Day) => {
+    let year = Day.getFullYear();
+    let month = Day.getMonth() + 1;
+    let date = Day.getDate();
+
+    let TodayDate = year + "" + month + "" + date;
+    console.log(TodayDate);
+  };
+
   return (
     <S.MiddleWrapper>
       <S.CalenderMain>
-        -
+        <Calendar value={value} onChange={onChange} onClickDay={callDay} />
       </S.CalenderMain>
     </S.MiddleWrapper>
   );
