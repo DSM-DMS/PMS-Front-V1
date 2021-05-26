b312fe20e69dee768d0c02ea888b9256704add66
Unknown <ljw041101@gmail.com>
2021-05-26 18:46:38 +0900

    [MODIFY] 파일 오류 수정

--- pms-front-v1/src/components/MyPage/MyPage.js
+++ pms-front-v1/src/components/MyPage/MyPage.js
@@ -2,34 +2,9 @@ import React from "react";
 import * as S from "./style";
 import BackgroundTitle from '../BackgroundTitle'
 import Footer from '../footer/Footer'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-import {ReactComponent as Profile} from '../../assets/Prifile.svg'
-import {ReactComponent as Bike} from '../../assets/Bike.svg'
-import {ReactComponent as Home} from '../../assets/Home.svg'
-import {ReactComponent as MealGreen} from '../../assets/MealGreen.svg'
-import {ReactComponent as RightPassword} from '../../assets/RightPassword.svg' 
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-import { Profile ,Bike, Home, MealGreen, RightPassword, Triangle, AddChildren, WrongPassword } from '../../assets/index'
-import UserInformation from './UserInformation/UserInformation'
-=======
 import { Profile ,Bike, Home, MealGreen, Triangle, AddChildren} from '../../assets/index'
 import UserInformation from './UserInformation/UserInformation'
 import ChangePassword from './ChangePassword/ChangePassword'
->>>>>>> Stashed changes
-<<<<<<< Updated upstream
-=======
-=======
-import { Profile ,Bike, Home, MealGreen, Triangle, AddChildren} from '../../assets/index'
-import UserInformation from './UserInformation/UserInformation'
-import ChangePassword from './ChangePassword/ChangePassword'
->>>>>>> Stashed changes
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 
 function MyPage() {
@@ -37,65 +12,13 @@ function MyPage() {
         <S.MainWrapper>
             <BackgroundTitle title="마이페이지" />
             <S.MyPageWrapper>
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-                <S.MyPageItem>
-                    <S.ItemBox>
-                        <S.UesrInformation>
-                            <h4>개인 정보</h4><br/>
-                            <S.UesrInformationItemTop>
-                                <div className="ItemTitle">이메일</div>
-                                <div className="ItemContent">ljw041101@gmail.com</div>
-                            </S.UesrInformationItemTop>
-                            <S.UesrInformationItemBottom>
-                                <div className="ItemTitle">닉네임</div>
-                                <input></input>
-                            </S.UesrInformationItemBottom>
-                        </S.UesrInformation><br/>
-=======
->>>>>>> Stashed changes
                 <div className="myPageItem">
                     <div className="itemBox">
                         <UserInformation />
                         <br/>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-                        <S.ChangePassword>
-                            <div className="changePasswordTitle"><h4>비밀번호 변경</h4>&nbsp;&nbsp;&nbsp;개인정보 보호를 위해 비밀번호는 주기적으로 변경해주세요.</div>
-                            <br/>
-                            <div className="changePasswordItem">
-                                <div className="changepassworditeminput"><input placeholder="현재 비밀번호 입력" type="password"></input><img src={RightPassword} alt=""></img></div>
-                                <div className="errormessage" id="nowPW-error">비밀번호가 일치하지 않습니다</div>
-                                <div className="changepassworditeminput"><input placeholder="신규 비밀번호 입력" type="password"></input></div>
-                                <div className="changepassworditeminput"><input placeholder="신규 비밀번호 재입력" type="password"></input><img src={WrongPassword} alt=""></img></div>
-                                <div className="errormessage">비밀번호가 일치하지 않습니다</div>
-                                <button>확인</button>
-                            </div>
-                        </S.ChangePassword>
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-                    </S.ItemBox>
-                    <S.ItemBox>
-=======
-=======
                         <ChangePassword />
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-=======
-                        <ChangePassword />
->>>>>>> Stashed changes
                     </div>
                     <div className="itemBox">
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
                         <h4>자녀정보</h4><br/>
                         <div className="childrenInformation">
                             <S.MyChildren>
--- pms-front-v1/src/components/MyPage/style.js
+++ pms-front-v1/src/components/MyPage/style.js
@@ -38,57 +38,6 @@ export const MyPageWrapper = styled.div`
         }
     }
 `;
-<<<<<<< Updated upstream
-export const ChangePassword = styled.div`
-    width: 100%;
-    height: 60%;
-    .changePasswordTitle{
-        display: flex;
-        font-size: 13px;
-        color: #9e9e9e;
-        h4{
-            font-size: 18px;
-            color: black;
-        }
-    }
-    .changePasswordItem{
-        width:90%;
-        height: 100%;
-        .changepassworditeminput{
-            width:100%;
-            height: 20%;
-            border-bottom: 1px solid #c8c8c8;
-            display: flex;
-            align-items: flex-end;
-        }
-        .errormessage{
-            color: red;
-            font-size: 13px;
-            display: none;
-        }
-        input{
-            width:95%;
-            height: 25%;
-            outline: none;
-            border: none;
-            color: #9e9e9e;
-            font-size:16px;
-            margin: 15px 0;
-        }
-        button{
-            width: 25%;
-            height: 12%;
-            background-color:#c8c8c8;
-            color: white;
-            border: none;
-            border-radius: 2px;
-            margin-top: 20px;
-            font-size: 16px;
-        }
-    }
-`
-=======
->>>>>>> Stashed changes
 export const MyChildren = styled.div`
     background-color: white;
     width: 95%;
