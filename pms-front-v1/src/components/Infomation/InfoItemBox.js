3858b1d9fc0b720231d0662780ef98b9047ed8e9
강은빈 <kub0803@naver.com>
2021-05-28 11:11:32 +0900

    [MODIFY] 동아리 리스트 목록 ITEM 수정

--- pms-front-v1/src/components/Infomation/InfoItemBox.js
+++ pms-front-v1/src/components/Infomation/InfoItemBox.js
@@ -4,15 +4,16 @@ import * as S from "./style";
 const InfoItemBox = (props) => {
   //모달 클릭시 나타나게 해줌
   const ItemBoxClickHandler = () => {
-    props.setModal("flex");
+    props.setModalBool();
+    props.setClubTitle(props.clubName);
   };
 
   return (
     <>
-      <S.ItemBox onClick={ItemBoxClickHandler}>
+      <S.ItemBox onClick={(e) => ItemBoxClickHandler(e)}>
         <S.ItemContainer>
           <img src={props.clubImg} alt="로고"></img>
-          <h3>{props.title}</h3>
+          <h3>{props.clubName}</h3>
           <p>{props.explanation}</p>
           <span>{props.resolution}</span>
         </S.ItemContainer>
