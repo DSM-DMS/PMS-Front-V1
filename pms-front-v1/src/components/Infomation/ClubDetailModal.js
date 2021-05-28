710b017c633b9136de7d5c4577f5c453d626549b
강은빈 <kub0803@naver.com>
2021-05-28 11:12:46 +0900

    [MODIFY] 동아리 상세 팝업 데이터

--- pms-front-v1/src/components/Infomation/ClubDetailModal.js
+++ pms-front-v1/src/components/Infomation/ClubDetailModal.js
@@ -27,12 +27,14 @@ const ClubDetailModal = (props) => {
   useEffect(() => {
     sliderRef.current.style.transition = "all 0.5s ease-in-out";
     sliderRef.current.style.transform = `translateX(-${currentSlider}0%)`;
+
     let i = 0;
     setInterval(() => {
       i++;
       setVal(i);
-    }, 2000);
-    if (i === 3) i = 0;
+
+      if (i === 4) i = 0;
+    }, 3000);
   }, [currentSlider]);
 
   return (
@@ -42,25 +44,25 @@ const ClubDetailModal = (props) => {
           <div className="background-cover">
             <div onClick={props.ModalClose} className="close-img" />
             <div className="club-wrapper">
-              <div className="logo"></div>
-              <span>DMS</span>
+              <img src={props.img} alt="로고" className="logo"></img>
+              <span>{props.clubName}</span>
             </div>
           </div>
         </S.ModalHeader>
         <div className="title-bar"></div>
         <S.InfoWrapper>
-          <h3>{props.titleName}</h3>
-          <span>jhgfd</span>
+          <h3>{props.explanation}</h3>
+          <span></span>
           <div
             className="club-img"
             ref={sliderRef}
             style={{ transform: `translateX(${-val * 250}px)` }}
           >
-            <img src="/" alt="동아리 사진"></img>
-            <img src="/" alt="동아리 사진"></img>
-            <img src="/" alt="동아리 사진"></img>
-            <img src="/" alt="동아리 사진"></img>
-            <img src="/" alt="동아리 사진"></img>
+            <img src={props.img} alt="동아리 사진"></img>
+            <img src={props.img} alt="동아리 사진"></img>
+            <img src={props.img} alt="동아리 사진"></img>
+            <img src={props.img} alt="동아리 사진"></img>
+            <img src={props.img} alt="동아리 사진"></img>
           </div>
           <div className="button-icon">
             <button onClick={prevSlider}>이전</button>
