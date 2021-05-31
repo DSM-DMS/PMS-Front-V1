0b364f9236f5d7f2d2e879eeb22770a1a969d287
강은빈 <kub0803@naver.com>
2021-05-31 23:06:27 +0900

    [MODIFY] <Calendar/Calendar>API 구조 수정

--- pms-front-v1/src/components/Calender/Calender.js
+++ pms-front-v1/src/components/Calender/Calender.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import * as S from "./style";
 
 import BackgroundTitle from "../BackgroundTitle";
@@ -6,56 +6,8 @@ import Footer from "../footer/Footer";
 import MonthEvent from "./MonthEvent";
 import MonthCalender from "./MonthCalender";
 import TodayMeals from "./TodayMeals";
-import { requestJW } from "../../utils/axios/axios";
 
 function Calender() {
-  const [eventData, setEventData] = useState([]);
-  const [eventDayData, seteEventDayData] = useState([]);
-
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-
-  /* if (month < 10) {
-    month = "0" + month;
-  }
-  if (day < 10) {
-    day = "0" + day;
-  }
-
-  let TodayDate = year + "-" + month + "-" + day;
-  console.log(TodayDate);
-
-  useEffect(() => {
-    const eventApi = async () => {
-      try {
-        const { data } = await requestJW(
-          "get",
-          "calendar",
-          {
-            Authorization: `Bearer ${localStorage.getItem("access-token")}`,
-          },
-          {}
-        );
-
-        setEventData(data[`${month}`]);
-        console.log(data);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    eventApi();
-  }, [month, TodayDate]);
-
-  const eventDate = Object.keys(eventData).reduce(
-    (prev, date) => prev.concat({ date, scheudles: eventData[date] }),
-    []
-  );
-
-  console.log(eventDate); */
-
   return (
     <S.MainWrapper>
       <BackgroundTitle title="" />
