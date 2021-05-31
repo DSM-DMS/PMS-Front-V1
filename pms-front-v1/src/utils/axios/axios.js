d36dd28f77eff8010a2011f6db96f795a51acd46
강은빈 <kub0803@naver.com>
2021-05-31 23:12:33 +0900

    [ADD] API 모듈 추가

--- pms-front-v1/src/utils/axios/axios.js
+++ pms-front-v1/src/utils/axios/axios.js
@@ -1,7 +1,19 @@
 import Axios from "axios";
-import { useHistory } from "react-router-dom";
 
-export const MainURL = "http://52.231.93.45:8080/";
+export const MainURL = "https://api.smooth-bear.live";
+export const JwURL = "http://api.potatochips.live/";
+
+const token = `${localStorage.getItem("access-token")}`;
+
+export const fetcher = (url) =>
+  Axios.get(url)
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
+
+export const authFetcher = (url) =>
+  Axios.get(url, { headers: { Authorization: "Bearer " + token } })
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
 
 export function request(method, url, header, data) {
   return Axios({
@@ -11,12 +23,26 @@ export function request(method, url, header, data) {
     data: data,
   })
     .then((res) => {
-      console.log("받기 성공");
       console.log(res);
       return res;
     })
     .catch((e) => {
-      console.log(MainURL + url);
-      console.error(e);
+      throw e;
+    });
+}
+
+export function requestJW(method, url, header, data) {
+  return Axios({
+    method,
+    url: JwURL + url,
+    headers: header,
+    data: data,
+  })
+    .then((res) => {
+      console.log(res);
+      return res;
+    })
+    .catch((e) => {
+      throw e;
     });
 }
