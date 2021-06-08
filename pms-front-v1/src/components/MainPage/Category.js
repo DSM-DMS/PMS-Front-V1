8c5626578d8f7fba20602b4d4d28b3eabc196595
강은빈 <kub0803@naver.com>
2021-06-08 20:25:37 +0900

    [DELETE] useHistory 삭제

--- pms-front-v1/src/components/MainPage/Category.js
+++ pms-front-v1/src/components/MainPage/Category.js
@@ -3,13 +3,10 @@ import * as S from "./style";
 import { useHistory } from "react-router-dom";
 import { SchoolInfo, Meals, ClubCategory } from "../index";
 import CompanyInfo from "./Category/CompanyInfo";
+import { Info } from "../../assets/index";
 
 const Category = () => {
   const history = useHistory();
-
-  const ClubClickHandler = () => {
-    history.push("club-info");
-  };
 
   const DMSClickHandler = () => {
     history.href("https://www.dsm-dms.com/");
@@ -19,61 +16,59 @@ const Category = () => {
     history.push("/login");
   };
 
-  const DeveloperClickHandler = () => {
-    history.push("/creators-info");
-  };
-
   return (
-    <S.CategoryWrapper>
-      <S.CategoryItem>
-        <SchoolInfo />
-        <ClubCategory onClick={ClubClickHandler} />
-        <CompanyInfo />
-        {/* DMS 구경하기 */}
-        <S.DMSInfo href="https://www.dsm-dms.com">
-          <S.DMSInfo onClick={DMSClickHandler} />
-        </S.DMSInfo>
-      </S.CategoryItem>
-      {/* 마이페이지, 로그인 묶음 */}
-      <S.CategoryItemBottom>
-        <S.StudentInfo>
-          <div className="container club">
-            <S.Title>마이페이지</S.Title>
-            <S.SubTitle>로그인 후 이용 가능합니다.</S.SubTitle>
-            <S.LoginButton onClick={LoginBtnClick}>로그인</S.LoginButton>
-          </div>
-          <div className="student-info-img"></div>
-        </S.StudentInfo>
+    <>
+      <S.CategoryWrapper>
+        <S.CategoryItem>
+          <SchoolInfo />
+          <ClubCategory />
+          <CompanyInfo />
+          {/* DMS 구경하기 */}
+          <S.DMSInfo href="https://www.dsm-dms.com">
+            <S.DMSInfo onClick={DMSClickHandler} />
+          </S.DMSInfo>
+        </S.CategoryItem>
+        {/* 마이페이지, 로그인 묶음 */}
+        <S.CategoryItemBottom>
+          <S.StudentInfo>
+            <div className="container club">
+              <S.Title>마이페이지</S.Title>
+              <S.SubTitle>로그인 후 이용 가능합니다.</S.SubTitle>
+              <S.LoginButton onClick={LoginBtnClick}>로그인</S.LoginButton>
+            </div>
+            <div className="student-info-img"></div>
+          </S.StudentInfo>
 
-        <Meals />
-        {/* 개발자 소개, PMS 소개  */}
-        <S.InfoWrapper>
-          <S.DeveloperInfo>
-            <div className="pms-info" onClick={DeveloperClickHandler}>
-              <S.Title>개발자 소개</S.Title>
-              <S.Font14>
-                PMS 학부모 서비스를 개발한 대덕소프트웨어마이스터고에 재학중인
-                학생들 소개입니다. 많은 응원과 칭찬을 나눠주세요. PMS를
-                이용해주셔서 감사합니다.
-              </S.Font14>
-            </div>
-            {/* 개발자 이미지 안뜸 */}
-            <div classNmae="creators-info"></div>
-          </S.DeveloperInfo>
-          <S.PMSInfo>
-            <div className="pms-info">
-              <S.Title>PMS 학부모 서비스</S.Title>
-              <S.Font14>
-                PMS 학부모 서비스는 가정통신문, 학교소식, 오늘의 급식, 학교의
-                동아리, 재학생의 취업처를 알 수 있을 뿐만 아니라 자녀의 기숙사의
-                정보를 알 수 있습니다.
-              </S.Font14>
-            </div>
-            <div className="pms-img"></div>
-          </S.PMSInfo>
-        </S.InfoWrapper>
-      </S.CategoryItemBottom>
-    </S.CategoryWrapper>
+          <Meals />
+          {/* 개발자 소개, PMS 소개  */}
+          <S.InfoWrapper>
+            <S.DeveloperInfo>
+              <a href="/creators-info">
+                <div className="pms-info">
+                  <S.Title>개발자 소개</S.Title>
+                  <S.Font14>
+                    PMS 학부모 서비스를 개발한 대덕소프트웨어마이스터고에
+                    재학중인 학생들 소개입니다. PMS를 이용해주셔서 감사합니다.
+                  </S.Font14>
+                </div>
+                <img src={Info} alt="개발자 소개"></img>
+              </a>
+            </S.DeveloperInfo>
+            <S.PMSInfo>
+              <div className="pms-info">
+                <S.Title>PMS 학부모 서비스</S.Title>
+                <S.Font14>
+                  PMS 학부모 서비스는 가정통신문, 학교소식, 오늘의 급식, 학교의
+                  동아리, 재학생의 취업처를 알 수 있을 뿐만 아니라 자녀의
+                  기숙사의 정보를 알 수 있습니다.
+                </S.Font14>
+              </div>
+              <div className="pms-img"></div>
+            </S.PMSInfo>
+          </S.InfoWrapper>
+        </S.CategoryItemBottom>
+      </S.CategoryWrapper>
+    </>
   );
 };
 
