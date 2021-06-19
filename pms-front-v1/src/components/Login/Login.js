ecf9db016153418a079e7348f79e9f0c9563581c
강은빈 <kub0803@naver.com>
2021-06-19 15:12:53 +0900

    [MODIFY] 로그인 회원가입

--- pms-front-v1/src/components/Login/Login.js
+++ pms-front-v1/src/components/Login/Login.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import * as S from "./style";
-
-import BackgroundTitle from "../BackgroundTitle";
-import SocialButton from "./SocialButton";
-import Footer from "../footer/Footer";
-import { request } from "../../utils/axios/axios";
+import { BackgroundTitle, SocialButton, Footer } from "../index";
+import { LoginError } from "../../assets";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../actions/userAction";
 
 function Login() {
+  const history = useHistory();
+  const [errorIcon, setErrorIcon] = useState("none");
+  const [borderBottom, setBorderBottom] = useState("");
+  const [width, setWidth] = useState("");
+
+  const dispatch = useDispatch();
+
   const [inputs, setInputs] = useState({
     email: "",
     password: "",
@@ -27,26 +33,23 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      await request(
-        "post",
-        "/auth",
-        { Authorization: `Bearer ${localStorage.getItem("access-token")}` },
-        {
-          email,
-          password,
-        }
-      );
-    } catch (e) {
-      console.log(e);
-    }
+    dispatch(loginUser(inputs))
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("access-token", res.payload.data["access-token"]);
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        setErrorIcon("flex");
+        setBorderBottom("2px solid #FF0000");
+        setWidth("100%");
+      });
 
     setInputs({
       email: "",
       password: "",
     });
-
-    console.log(inputs);
   };
 
   return (
@@ -57,7 +60,7 @@ function Login() {
         <S.MainItem onSubmit={handleSubmit}>
           <S.Title>
             <p>PMS 로그인</p>
-            <Link to="sign-up">회원가입 하기 > </Link>
+            <Link to="sign-up">회원가입 하기 {">"} </Link>
           </S.Title>
           {/* 로그인 입력창 */}
           <S.LoginInput>
@@ -65,17 +68,34 @@ function Login() {
               <input
                 onChange={onChange}
                 name="email"
-                type="text"
+                type="email"
                 placeholder="이메일"
                 value={email}
               ></input>
-              <input
-                onChange={onChange}
-                name="password"
-                type="password"
-                placeholder="비밀번호"
-                value={password}
-              ></input>
+              <div
+                className="password-wrapper"
+                style={{ borderBottom: borderBottom }}
+              >
+                <input
+                  style={{ borderBottom: borderBottom }}
+                  onChange={onChange}
+                  name="password"
+                  type="password"
+                  placeholder="비밀번호"
+                  value={password}
+                ></input>
+                <img
+                  style={{ display: errorIcon }}
+                  src={LoginError}
+                  alt="로그인 오류"
+                ></img>
+              </div>
+              <span
+                className="error-message"
+                style={{ width: width, display: errorIcon }}
+              >
+                이메일 혹은 비밀번호를 다시 입력해주세요.
+              </span>
               <div className="auto-login">
                 <input type="checkbox"></input>
                 <span>자동 로그인</span>
@@ -92,7 +112,7 @@ function Login() {
           </S.LoginInput>
         </S.MainItem>
       </S.LoginWrapper>
-      <Footer></Footer>
+      <Footer />
     </S.MainWrapper>
   );
 }
